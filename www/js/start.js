011394da974b2707fec1a614ede44b7404929cb8
KanadeNishizawa <kanade27@icloud.com>
2020-02-29 02:34:11 +0900

    -

--- www/js/start.js
+++ www/js/start.js
@@ -7,16 +7,6 @@ export class Start extends createjs.MovieClip {
     this.bmp1.scaleY = (window.innerWidth + 100) / 720;
     this.addChild(this.bmp1);
 
-    //tap to start
-    // this.text = new createjs.Bitmap("./images/taptostart.png");
-    // this.text.scaleX = 300 / 1200;
-    // this.text.scaleY = 300 / 1200;
-    // this.text.regX = 600;
-    // this.text.regY = 180;
-    // this.text.x = window.innerWidth / 2;
-    // this.text.y = window.innerHeight / 1.5;
-    // this.addChild(this.text);
-
     // タイトルと説明文
     this.title = new createjs.Bitmap("./images/title.png");
     this.title.scaleX = (window.innerWidth * 0.9) / 1200;
