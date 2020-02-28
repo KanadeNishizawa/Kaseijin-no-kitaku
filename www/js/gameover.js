a33016e552f200a8f6f08c79d028976d55f9695b
KanadeNishizawa <kanade27@icloud.com>
2020-02-29 02:16:57 +0900

    スタート画面、ゲームオーバー画面変更

--- www/js/gameover.js
+++ www/js/gameover.js
@@ -6,6 +6,23 @@ export class Gameover extends createjs.MovieClip {
     this.bg.graphics.beginFill("#333333").drawRect(0, 0, w, h);
     this.addChild(this.bg);
 
+    // 火星人
+    this.bmp2 = new createjs.Bitmap("./images/top-player.png");
+    this.bmp2.regY = 220;
+    this.bmp2.regX = 120;
+    this.bmp2.y = window.innerHeight / 2;
+    this.bmp2.x = window.innerWidth / 2;
+    this.bmp2.rotation = 170;
+    this.bmp2.scaleX = window.innerHeight / 300;
+    this.bmp2.scaleY = window.innerHeight / 300;
+    this.addChild(this.bmp2);
+
+    // 背景を暗く
+    this.bg = new createjs.Shape();
+    this.bg.graphics.beginFill("#000").drawRect(0, 0, w, h);
+    this.bg.alpha = 0.7;
+    this.addChild(this.bg);
+
     //文字をかくよ
     this.text = new createjs.Bitmap("./images/gameover.png");
     this.text.scaleX = 350 / 1200;
@@ -13,7 +30,7 @@ export class Gameover extends createjs.MovieClip {
     this.text.regX = 600;
     this.text.regY = 100;
     this.text.x = window.innerWidth / 2;
-    this.text.y = window.innerHeight / 2.4;
+    this.text.y = window.innerHeight / 2.2;
     this.addChild(this.text);
   }
   render(score) {
@@ -25,7 +42,7 @@ export class Gameover extends createjs.MovieClip {
     this.text2.textAlign = "center";
     this.text2.textBaseline = "top";
     this.text2.x = window.innerWidth / 2;
-    this.text2.y = window.innerHeight / 2.1;
+    this.text2.y = window.innerHeight / 2;
     this.addChild(this.text2);
     document.addEventListener("click", () => location.reload());
   }
